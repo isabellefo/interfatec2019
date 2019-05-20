542b259d35a3986670673afe4835ecee5afc7c0c
Gabriel Borges <gabrielfr.borges@gmail.com>
2019-05-20 20:14:05 -0300

    Algoritmo para achar primo vai até sqrt(x)

--- digitos.py
+++ digitos.py
@@ -11,13 +11,11 @@ for j in range(n):
     print('INTERVALO {}'.format(j+1))
     for x in range(ab[j][0], ab[j][1]+1):
         c = 0
-        for i in range(1, x//2 +1):
+        for i in range(2, ( int(x**0.5) ) +1):
             if x%i == 0:
                 c += 1
-        if c  == 1:
+        if c  == 0 and x != 1 :
             l.append(str(x))
-    
     junto = ''.join(l)
-
     for x in range(10):
         print('{}: {}'. format(x, junto.count(str(x)) ))
